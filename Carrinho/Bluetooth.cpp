7cc8e2d93bb80c35bb788db79fbf06eba612e087
fsmiamoto <shigueo.miamoto@gmail.com>
2019-06-22 13:17:51 -0300

    Added readJoystick method (beta)

--- Carrinho/Bluetooth.cpp
+++ Carrinho/Bluetooth.cpp
@@ -16,7 +16,7 @@ String Bluetooth::read()
     char inChar = "";
     if(BTSerial.available()){
         while(BTSerial.available()){
-            inChar = (char)BTSerial.read(); //read the input
+            inChar = (char) BTSerial.read(); //read the input
             inputString += inChar;          //make a string of the characters coming on serial
         }
         while (BTSerial.available() > 0){
@@ -26,3 +26,39 @@ String Bluetooth::read()
   return inputString;
 }
 
+String Bluetooth::readJoystick(short* returnValues){
+    char incomingChar = "";
+    char firstChar = "";
+    String messageString = "";
+
+    if(BTSerial.available()){
+        delay(2);
+        firstChar = (char) BTSerial.read();
+        if(firstChar == START_TX){
+            while(BTSerial.available()){
+                    delay(1);
+                    incomingChar = BTSerial.read();
+                    // Greater than 127 means a communication error
+                    if(incomingChar == END_TX || incomingChar > 127 )
+                        break;
+                    messageString += (char) incomingChar;
+            }
+            while (BTSerial.available() > 0){
+                BTSerial.read() ; //clear the buffer
+            } 
+        }
+        else 
+            return;
+    }
+
+    if(messageString.length() == 6) {
+
+        short x = messageString.substring(0,3).toInt() - 200;
+        short y = messageString.substring(4,6).toInt() - 200;
+
+        returnValues[0] = x;
+        returnValues [1] = y;
+    }
+
+    return messageString;
+}
--- Carrinho/Bluetooth.h
+++ Carrinho/Bluetooth.h
@@ -1,12 +1,16 @@
 #pragma once
 #include <SoftwareSerial.h>
 
+#define START_TX 0x02
+#define END_TX 0x03
+
 class Bluetooth{
     SoftwareSerial BTSerial;
 
     public:
         Bluetooth(short rxPin, short txPin);
         String read();
+        String readJoystick(short* returnValues);
         void begin(int baudRate);
 };
 
