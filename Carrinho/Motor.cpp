38ceaf6a2fc1560e4e7e8a5cd3e1bddeb541da63
Francisco Miamoto <shigueo.miamoto@gmail.com>
2019-06-26 19:06:49 -0300

    Danilo: Fix motor speed problems (int casting)

--- Carrinho/Motor.cpp
+++ Carrinho/Motor.cpp
@@ -2,7 +2,7 @@
 
 #define abs(x) ((x) > 0 ? (x) : -(x))
 
-long map(long x, long in_min, long in_max, long out_min, long out_max) {
+float map(float x, float in_min, float in_max, float out_min, float out_max) {
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
@@ -20,19 +20,21 @@ Motor::Motor(short leftMotorNumber, short rightMotorNumber,
  */
 void Motor::setMovement(short speed, short direction) //
 {
-  int x = direction;
-  int y = speed;
+  float x = direction;
+  float y = speed;
 
   // TODO: search for the link
-  int16_t v = (100 - abs(x)) * (y / 100) + y;
-  int16_t w = (100 - abs(y)) * (x / 100) + x;
+  float v = (100 - abs(x)) * (y / 100) + y; // 140
+  float w = (100 - abs(y)) * (x / 100) + x; // 0
 
-  int16_t left = (v + w) / 2;
-  int16_t right = (v - w) / 2;
+  float leftPercentage = (v + w) / 2;  // 70
+  float rightPercentage = (v - w) / 2; // 70
 
   // maps each value on the motor's scale
-  left = leftMultiplier * map(left, -100, 100, -255, 255);
-  right = rightMultiplier * map(right, -100, 100, -255, 255);
+  int16_t left =
+      (int16_t)(leftMultiplier * map(leftPercentage, -100, 100, -255, 255));
+  int16_t right =
+      (int16_t)(rightMultiplier * map(rightPercentage, -100, 100, -255, 255));
 
   // sets the motors' direction
   if (right > 0) {
