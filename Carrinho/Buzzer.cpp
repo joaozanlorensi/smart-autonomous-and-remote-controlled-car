d19aeb205b92db26eb4e377589420670be7a73a5
Francisco Miamoto <shigueo.miamoto@gmail.com>
2019-07-03 21:11:27 -0300

    Added retry count and initial burst

--- Carrinho/Buzzer.cpp
+++ Carrinho/Buzzer.cpp
@@ -1,8 +1,26 @@
 #include "Buzzer.h"
 
-BUZZER::BUZZER(short int pin, int song) {
+const int Buzzer::victorySongMelody[] = {
+    NOTE_C4, NOTE_C5, NOTE_A3, NOTE_A4, NOTE_AS3, NOTE_AS4, 0, 0,
+};
+const int Buzzer::victorySongTempo[] = {
+    12, 12, 12, 12, 12, 12, 6, 6,
+};
+
+const int Buzzer ::challengeSongMelody[] = {
+    NOTE_D4,
+    NOTE_DS4,
+    NOTE_E4,
+};
+const int Buzzer::challengeSongTempo[] = {
+    6,
+    12,
+    2,
+};
+
+Buzzer::Buzzer(short int outPin, int song) {
   this->melodyPin = outPin;
-  pinMode(pin, OUTPUT);
+  pinMode(outPin, OUTPUT);
   this->song = song;
 }
 
@@ -15,7 +33,7 @@ void Buzzer::sing(int s) {
 
       // to calculate the note duration, take one second
       // divided by the note type.
-      //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+      // e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
       int noteDuration = 1000 / victorySongTempo[thisNote];
 
       buzz(melodyPin, victorySongMelody[thisNote], noteDuration);
@@ -34,7 +52,7 @@ void Buzzer::sing(int s) {
 
       // to calculate the note duration, take one second
       // divided by the note type.
-      //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+      // e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
       int noteDuration = 1000 / challengeSongTempo[thisNote];
 
       buzz(melodyPin, challengeSongMelody[thisNote], noteDuration);
@@ -46,24 +64,25 @@ void Buzzer::sing(int s) {
 
       // stop the tone playing:
       buzz(melodyPin, 0, noteDuration);
-
     }
   }
 }
 
 void Buzzer::buzz(int targetPin, long frequency, long length) {
-  digitalWrite(13, HIGH);
-  long delayValue = 1000000 / frequency / 2; // calculate the delay value between transitions
-  //// 1 second's worth of microseconds, divided by the frequency, then split in half since
-  //// there are two phases to each cycle
-  long numCycles = frequency * length / 1000; // calculate the number of cycles for proper timing
-  //// multiply frequency, which is really cycles per second, by the number of seconds to
-  //// get the total number of cycles to produce
+  long delayValue =
+      1000000 / frequency / 2; // calculate the delay value between transitions
+  //// 1 second's worth of microseconds, divided by the frequency, then split in
+  /// half since / there are two phases to each cycle
+  long numCycles = frequency * length /
+                   1000; // calculate the number of cycles for proper timing
+  //// multiply frequency, which is really cycles per second, by the number of
+  /// seconds to / get the total number of cycles to produce
   for (long i = 0; i < numCycles; i++) { // for the calculated length of time...
-    digitalWrite(targetPin, HIGH); // write the buzzer pin high to push out the diaphram
+    digitalWrite(targetPin,
+                 HIGH); // write the buzzer pin high to push out the diaphram
     delayMicroseconds(delayValue); // wait for the calculated delay value
-    digitalWrite(targetPin, LOW); // write the buzzer pin low to pull back the diaphram
+    digitalWrite(targetPin,
+                 LOW); // write the buzzer pin low to pull back the diaphram
     delayMicroseconds(delayValue); // wait again or the calculated delay value
   }
-  digitalWrite(13, LOW);
 }
--- Carrinho/Carrinho.ino
+++ Carrinho/Carrinho.ino
@@ -10,11 +10,11 @@
  */
 
 #include "Bluetooth.h"
+#include "Buzzer.h"
 #include "IR.h"
 #include "Motor.h"
 #include "RFID.h"
 #include "Ultrasonic.h"
-#include "Buzzer.h"
 
 #define LEFT_MOTOR_NUMBER 4
 #define RIGHT_MOTOR_NUMBER 3
@@ -32,18 +32,19 @@
 #define RFID_RST_PIN 9
 #define RFID_SS_PIN 10
 
-#define BUZZER_PIN 22 // Joao: *redefine pin
+#define BUZZER_PIN A5
 
 #define MODE_MANUAL 0
 #define MODE_AUTONOMOUS 1
 
 #define STEP_NULL 0
-#define STEP_SEARCH_TARGET 1
-#define STEP_GOTO_TARGET 2
-#define STEP_READ_RFID 3
-#define STEP_RETRY_RFID_BACKWARDS 4
-#define STEP_RETRY_RFID_SWIVEL 5
-#define STEP_LEAVE_ZONE 6
+#define STEP_INITIAL_BURST 1
+#define STEP_SEARCH_TARGET 2
+#define STEP_GOTO_TARGET 3
+#define STEP_READ_RFID 4
+#define STEP_RETRY_RFID_BACKWARDS 5
+#define STEP_RETRY_RFID_SWIVEL 6
+#define STEP_LEAVE_ZONE 7
 
 #define TURNING_RIGHT -1
 #define TURNING_LEFT 1
@@ -51,15 +52,17 @@
 #define MANUAL_SPEED 100
 #define MANUAL_TURNING_SPEED 80
 
-#define AUTO_SPEED 60
+#define AUTO_SPEED 70
 #define AUTO_BACKWARDS_SPEED -80
 #define AUTO_TURNING_SPEED 70
 
 #define FAR_DISTANCE_THRESHOLD 50
-#define CLOSE_DISTANCE_THRESHOLD 5
+#define CLOSE_DISTANCE_THRESHOLD 5.5
 #define RFID_READ_TIMEOUT 500
-#define RFID_BACKWARDS_DURATION 800
+#define RFID_BACKWARDS_DURATION 600
 #define RFID_SWIVEL_DURATION 400
+#define INITIAL_BURST_DURATION 100
+#define MAX_RETRY_COUNT 5
 
 #define BUZZER_SONG 0
 
@@ -69,6 +72,7 @@ typedef struct {
   short step;
   unsigned long timeRef;
   short searchDirection;
+  short retryCount;
 } AutonomousState;
 
 AutonomousState autonomousState;
@@ -92,17 +96,18 @@ void setup() {
   autonomousState.step = STEP_NULL;
   autonomousState.timeRef = millis();
   autonomousState.searchDirection = TURNING_LEFT;
+  autonomousState.retryCount = 0;
 }
 
 void loop() {
   if (ir.didReadingChange()) {
-    motor.setMovement(0, 0);
     if (ir.isBlack()) {
-      buzzer.sing(THEME_CHALLENGE); 
       mode = MODE_AUTONOMOUS;
-      autonomousState.step = STEP_SEARCH_TARGET;
+      autonomousState.step = STEP_INITIAL_BURST;
+      autonomousState.timeRef = millis();
     } else {
       mode = MODE_MANUAL;
+      motor.setMovement(0, 0);
       autonomousState.step = STEP_NULL;
     }
   }
@@ -149,14 +154,24 @@ AutonomousState autonomousControl(AutonomousState state) {
   short currentStep = state.step;
   unsigned long timeRef = state.timeRef;
   short searchDirection = state.searchDirection;
+  short retryCount = state.retryCount;
 
   short nextStep = STEP_NULL;
   unsigned long nextTimeRef = timeRef;
   short nextSearchDirection = searchDirection;
+  short nextRetryCount = retryCount;
 
   if (currentStep == STEP_NULL) {
     // Woops
     nextStep = STEP_NULL;
+  }
+  if (currentStep == STEP_INITIAL_BURST) {
+    if (millis() - timeRef > INITIAL_BURST_DURATION) {
+      nextStep = STEP_SEARCH_TARGET;
+    } else {
+      motor.setMovement(AUTO_SPEED, 0);
+      nextStep = STEP_INITIAL_BURST;
+    }
   }
   if (currentStep == STEP_SEARCH_TARGET) {
     float distance = ultrasonic.getDistance();
@@ -191,12 +206,18 @@ AutonomousState autonomousControl(AutonomousState state) {
     String tagUID = rfid.readUIDFromTag();
     bool successfulReading = tagUID != "";
     if (successfulReading) {
-      buzzer.sing(THEME_VICTORY); 
+      buzzer.sing(THEME_VICTORY);
       nextStep = STEP_LEAVE_ZONE;
     } else {
       if ((millis() - timeRef) > RFID_READ_TIMEOUT) {
-        nextStep = STEP_RETRY_RFID_BACKWARDS;
-        nextTimeRef = millis();
+        if (retryCount < MAX_RETRY_COUNT) {
+          nextStep = STEP_RETRY_RFID_BACKWARDS;
+          nextRetryCount = retryCount + 1;
+          nextTimeRef = millis();
+        } else {
+          nextStep = STEP_LEAVE_ZONE;
+          nextRetryCount = 0;
+        }
       } else {
         nextStep = STEP_READ_RFID;
       }
@@ -227,5 +248,6 @@ AutonomousState autonomousControl(AutonomousState state) {
 
   return (AutonomousState){.step = nextStep,
                            .timeRef = nextTimeRef,
-                           .searchDirection = nextSearchDirection};
+                           .searchDirection = nextSearchDirection,
+                           .retryCount = nextRetryCount};
 }
